1bc58ce9dfb656555bc5f975380690971773edf0
Samuel <Samkostofik@gmail.com>
2025-03-28 21:14:23 +0100

    added DenneUlohy component

---
+++ kaufland_system_web_api.client/src/Components/DenneUlohy/DenneUlohy.tsx
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7145/api/Product';
+
+
+const DenneUlohy: React.FC = () => {
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`${API_URL}/NulovaZasoba`);
+            const data = response.data;
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+
+
+    return (
+        <>
+            <h1>DenneUlohy</h1>
+
+            <p>Artikle s nulovou zasobou: </p>
+
+            <p>Artikle s minusovou zasobou: </p>
+
+            <p>AS proces </p>
+
+            <p>Datumy Spotreby:</p>
+
+            <p>Zlavy s centraly</p>
+        </>
+    )
+}
+export default DenneUlohy;
